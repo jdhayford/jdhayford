c74144fac98e6f3405cb51eaaaa102b8f277c137
Jack Hayford <jdhayford@gmail.com>
2020-07-21 11:57:44 -0400

    Add GA tracking code

--- src/js/utils/init.js
+++ src/js/utils/init.js
@@ -3,7 +3,7 @@ import ReactGA from 'react-ga'
 export const initializeDep = () => {
 
     if (process.env.NODE_ENV === 'production') {
-        ReactGA.initialize()
+        ReactGA.initialize('UA-167736503-3')
         ReactGA.pageview(window.location.pathname + window.location.search)
     }
 }
